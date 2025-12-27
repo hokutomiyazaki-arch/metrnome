e66c2bb5e5db0e9992ec81eebf8c0ee29280b72c
Functional Neuro Training <hokutomiyazaki312@gmail.com>
2025-12-27 22:44:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fnt-metronome-v2';
+const CACHE_NAME = 'fnt-metronome-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -7,40 +7,31 @@ const ASSETS = [
     './FNT512-transparent.png'
 ];
 
-// インストール
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(ASSETS))
+            .then(cache => cache.addAll(ASSETS))
             .then(() => self.skipWaiting())
     );
 });
 
-// アクティブ化（古いキャッシュ削除）
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys()
-            .then((cacheNames) => {
-                return Promise.all(
-                    cacheNames
-                        .filter((name) => name !== CACHE_NAME)
-                        .map((name) => caches.delete(name))
-                );
-            })
+            .then(names => Promise.all(
+                names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+            ))
             .then(() => self.clients.claim())
     );
 });
 
-// フェッチ（Network First）
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         fetch(event.request)
-            .then((response) => {
+            .then(response => {
                 if (response && response.status === 200) {
-                    const responseClone = response.clone();
-                    caches.open(CACHE_NAME).then((cache) => {
-                        cache.put(event.request, responseClone);
-                    });
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
                 }
                 return response;
             })
